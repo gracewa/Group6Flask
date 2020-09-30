e2bc1f92758b1d571d0ad92430bfcaadaa6c1997
gracewa <33077294+gracewa@users.noreply.github.com>
2020-09-30 10:09:29 +0300

    manage.py and start.sh

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,15 @@ from flask_login import LoginManager
 
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 
-
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
